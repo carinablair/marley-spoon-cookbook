afdd10cb48f936d22be8201e1caed339388c90f1
Carina Blair <cdblairnz@gmail.com>
2021-10-19 18:29:57 +0200

    Implement basic data flow to display a list of recipe titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { createClient } from 'contentful';
 
 function App() {
+  const [recipes, setRecipes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const client = createClient({
+      space: process.env.REACT_APP_SPACE_ID!,
+      environment: process.env.REACT_APP_ENVIRONMENT_ID,
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN!
+    })
+
+    client.getEntries({
+      content_type: 'recipe'
+    })
+      .then((response) => {
+        setRecipes(response.items)
+        console.log(response.items)
+      })
+      .catch(console.error)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Recipes</h2>
+      <ul>{recipes.map(recipe => <li>{recipe.fields.title}</li>)}</ul>
     </div>
   );
 }
