17764995ea76dd910efd236c39eb3c6cbf307455
Carina Blair <cdblairnz@gmail.com>
2021-10-24 13:19:21 +0200

    Style the recipe details

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { getRecipe } from "../Api";
 import { RecipeDetails } from "../Types";
+import ReactMarkdown from 'react-markdown'
 
 interface RecipeProps {
     recipeId: string;
@@ -23,24 +24,25 @@ function Recipe(props: RouteComponentProps<RecipeProps>) {
         return <span>Loading...</span>
     }
     return (
-        <>
-            <span key='title'>{recipe.title}</span>
-            <img src={`${recipe.imageUrl}?h=400`}
+        <div className='recipe-details'>
+            <img src={`${recipe.imageUrl}?w=600`}
                 alt={recipe.title} />
+            <h1>{recipe.title}</h1>
             {recipe.tags?.map(
                 (tag) => (
-                    <span key='tag'>
+                    <span key={tag} className='tag'>
                         {tag}
                     </span>
                 ))
             }
-            <span key='description'>
+            <h2>What's cooking</h2>
+            <ReactMarkdown>
                 {recipe.description}
-            </span>
+            </ReactMarkdown>
             <span key='chef'>
                 {recipe.chef}
             </span>
-        </>
+        </div>
     )
 
 }
